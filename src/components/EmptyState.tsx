1d45fc376cb1ed48bd346fca33411c4e7a05ff4b
Mike Chen <1325549+mhchen@users.noreply.github.com>
2025-02-19 22:26:43 -0800

    Don't optimize EmptyState image

--- src/components/EmptyState.tsx
+++ src/components/EmptyState.tsx
@@ -5,7 +5,7 @@ export default function EmptyState({ children }: { children: ReactNode }) {
   return (
     <div className="flex flex-col items-center py-12 gap-12">
       <div className="w-full max-w-60 aspect-square relative">
-        <Image src="/empty-state.svg" alt="" fill />
+        <Image src="/empty-state.svg" alt="" fill unoptimized />
       </div>
       <div>
         {children}
